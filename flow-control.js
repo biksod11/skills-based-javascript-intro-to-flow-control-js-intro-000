fe6b831aa3ffc364cb724f79557a2cc810acc0de
Bikram Sodhi <biksodhi@gmail.com>
2020-01-16 18:19:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,12 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19)
   return "You are a teenager!"
+} else "You are not a teenager"
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19)
+  return "You are a teenager!"
 }
 
 function ageChecker(age) {
